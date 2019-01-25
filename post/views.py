50e9ead3927bf05dc7980f5231a287a5a88d4451
Adriano Damasceno da Silva Júnior <adamascenosj@gmail.com>
2019-01-25 17:37:11 -0200

    Adapted app post

--- post/views.py
+++ post/views.py
@@ -4,10 +4,8 @@ from .models import Post
 
 # Create your views here.
 def post_render(request, id):
-    print("\n\n\n\n %d \n\n\n\n\n" %(id))
     post = get_object_or_404(Post, pk=id)
-    print("\n\n\n\n", post.authors, "\n\n\n\n")
-    return render(request, 'post.html', {'post' : post})
+    return render(request, 'post.html', {'post' : post, 'authors': post.authors.all()})
 
 
 
