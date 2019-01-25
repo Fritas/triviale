a76cb21b1d6956c458d623179d7e6d6a5a29e14a
Adriano Damasceno da Silva Júnior <adamascenosj@gmail.com>
2019-01-25 18:09:53 -0200

    Adapted app author

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_render
+from .views import post_render, author_render
 
 urlpatterns = [
     path('<int:id>/', post_render, name='post'),
+    path('author/<int:id>', author_render, name='author')
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Author
 
 
 # Create your views here.
@@ -8,4 +8,8 @@ def post_render(request, id):
     return render(request, 'post.html', {'post' : post, 'authors': post.authors.all()})
 
 
+def author_render(request, id):
+    author = get_object_or_404(Author, pk=id)
+    posts = author.post_set.all()
+    return render(request, 'author.html', {'author' : author, 'posts' : posts})
 
